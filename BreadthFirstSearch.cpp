04af3a6557a693f8183f4c7d0303dede396cc020
Martin Grund <grundprinzip@gmail.com>
2013-12-13 13:36:03 +0100

    Next

--- BreadthFirstSearch.cpp
+++ BreadthFirstSearch.cpp
@@ -1,19 +1,26 @@
 #include "BreadthFirstSearch.h"
 
-#include "access/system/QueryParser.h"
+#include <access/system/PlanOperation.h>
+#include <access/system/QueryParser.h>
 
 namespace hyrise { namespace access {
+
+  
   namespace { 
-    auto _ = QueryParser::registerPlanOperation<BreadthFirstSearch>("BFS");
+    auto _ = QueryParser::registerPlanOperation<BreadthFirstSearchIdx>("BFS");
   }
 
-
-  void BreadthFirstSearch::executePlanOperation() {
+  void BreadthFirstSearchIdx::executePlanOperation() {
 
   }
 
-  std::shared_ptr<PlanOperation> BreadthFirstSearch::parse(const Json::Value &data) {
-    auto res = BasicParser<IndexJoin>::parse(data);
+  void BreadthFirstSearchIdx::setIndexName(std::string n) {
+    _indexName = n;
+  }
+
+  std::shared_ptr<PlanOperation> BreadthFirstSearchIdx::parse(const Json::Value &data) {
+    auto res = BasicParser<BreadthFirstSearchIdx>::parse(data);
+    res->setIndexName(data["index"].asString());
     return res;
   }
 
--- BreadthFirstSearch.h
+++ BreadthFirstSearch.h
@@ -1,13 +1,30 @@
 // Copyright (c) 2013 Hasso-Plattner-Institut fuer Softwaresystemtechnik GmbH. All rights reserved.
 #pragma once
 
+#include <access/system/PlanOperation.h>
+
 namespace hyrise { namespace access {
 
 /**
+*  Allows to perform a breadth first search through a graph using an index and
+*  the indexed field. The operation assumes that for each node the outlinks
+*  can be obtained by looking up the values from the index.
 *
+*  Currently we assume that IDs are Integer values
 */
 class BreadthFirstSearchIdx : public PlanOperation {
 
+public:
+
+  // Default value for break at level x
+  static const size_t MAX_LEVEL = 5;
+
+protected:
+
+  // The index representing the graph
+  std::string _indexName;
+
+  size_t _levelStop = MAX_LEVEL;
 
 
 public:
@@ -16,7 +33,8 @@ public:
 
   void executePlanOperation();
 
+  void setIndexName(std::string idx);
 
 };
 
-}
+}}
